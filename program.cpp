3eeb2b3dd14bc798a88f86f02bd49be9a7dd7836
mrcconaway <mrcconaway@gmail.com>
2022-05-07 13:41:47 -0400

    testing

--- program.cpp
+++ program.cpp
@@ -1,19 +1,26 @@
 #include "program.h"
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 program::program(int argc, char* argv[] )
 {
-    if(argc < 2){
-        program();
-    }
-    else{
-	    for(int i = 1; i < argc; ++i){
+    // if(argc < 2){
+    //     program();
+    // }
+    // else{
+        std::cout << endl;
+	    for(int i = 0; i < argc; ++i){
+            std::cout << i << " " <<  argv[i] << endl;
 		    if(i == 1) {csv.f = true; csv.s = argv[i];}
 		    if(i == 2) {roster.f = true; roster.s = argv[i];}
 		    if(i == 3) {output.s = true; output.s = argv[i];}
 	    }
-    }
+    // }
+        std::cout << endl;
+        std::string tmp;
+        std::getline(std::cin, tmp);
+
 }
 
 program::program()
